493ac9fc1e56b60f50dad53959edca5d84641d1a
anubh-v <e0176401@u.nus.edu>
2019-01-19 18:03:48 +0800

    Add MISC STOPWORDS and calculate frequency

--- modules/extract.py
+++ modules/extract.py
@@ -1,15 +1,16 @@
 from psaw import PushshiftAPI
 import datetime as dt
+import nltk
 import json
 from tokenizer import tokenize
 
 api = PushshiftAPI()
 
 start = int(dt.datetime(2019, 1, 1).timestamp())
-end = int(dt.datetime(2019, 1, 2).timestamp())
+end = int(dt.datetime(2019, 1, 19).timestamp())
 
 
-gen = api.search_submissions(after=start, before=end, subreddit='worldnews')
+gen = api.search_submissions(after=start, before=end, subreddit='singapore')
 results = list(gen)
 
 print(len(results))
@@ -19,8 +20,11 @@ data['titles'] = []
 
 for result in results:
     print(result.title)
-    data['titles'].append(tokenize(result.title))
+    data['titles'].extend(tokenize(result.title))
     print(result.selftext)
+
+freq = nltk.FreqDist(data['titles'])
+print(freq.most_common(50))
 
 with open('results.json', 'w') as outfile:
     json.dump(data, outfile)
--- modules/tokenizer.py
+++ modules/tokenizer.py
@@ -3,14 +3,20 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import LancasterStemmer
 import string
 
-
 # return an array of tokens (with stopwords and punctuation filtered, stemed)
 def tokenize(sentence):
+    
+    MISC_STOPWORDS = ["'s", "http", "https"]
+     
     tokens = word_tokenize(sentence)
 
-    stop_words = set(stopwords.words('english'))
+    tokens = [w.lower() for w in tokens]
+
+    stop_words = list(stopwords.words('english')) + MISC_STOPWORDS
+    print(stop_words)
     tokens = [w for w in tokens if not w in stop_words]
 
-    punctSet = set(string.punctuation)
+    punctSet = list(string.punctuation)
     tokens = [w for w in tokens if not w in punctSet]
+
     return tokens
