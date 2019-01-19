15f5ad3e112d6a04033da6dbbcba4270edff496d
anubh-v <e0176401@u.nus.edu>
2019-01-19 17:10:19 +0800

    Write query results to JSON file

--- modules/extract.py
+++ modules/extract.py
@@ -1,18 +1,27 @@
 from psaw import PushshiftAPI
 import datetime as dt
+import json
+
 api = PushshiftAPI()
 
-start = int(dt.datetime(2019, 1, 1, 0).timestamp())
-end = int(dt.datetime(2019, 1, 1, 2).timestamp())
+start = int(dt.datetime(2019, 1, 1).timestamp())
+end = int(dt.datetime(2019, 1, 2).timestamp())
 
 
-gen = api.search_submissions(after=start, before=end, subreddit='politics')
+gen = api.search_submissions(after=start, before=end, subreddit='worldnews')
 results = list(gen)
 
 print(len(results))
 
+data = {}
+data['titles'] = []
+
 for result in results:
   print(result.title)
-  # print(result.selftext)
+  data['titles'].append(result.title)
+  print(result.selftext)
+
+with open('results.json', 'w') as outfile:
+    json.dump(data, outfile)
 
 
