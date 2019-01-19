13673fd9d774dd7b683eb16398b70e2fdd34dd12
jhengy <35480886+jhengy@users.noreply.github.com>
2019-01-19 22:24:07 +0800

    Integrating template in flask app

--- web/app.py
+++ web/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
 from modules.extract import extract_data
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def entry():
-    return extract_data()
-
+    data = extract_data()
+    return render_template("index.html", data=data)
+    # return extract_data()
