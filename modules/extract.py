45cd3756847b2ce1ac6e3174adf9c5250c031b90
jhengy <35480886+jhengy@users.noreply.github.com>
2019-01-19 22:20:02 +0800

    Auto formatting by ide formatter

--- modules/extract.py
+++ modules/extract.py
@@ -5,25 +5,26 @@ import json
 from modules.tokenizer import tokenize
 from modules.objectify import make_object_array
 
+
 def extract_data():
-  api = PushshiftAPI()
+    api = PushshiftAPI()
 
-  start = int(dt.datetime(2018, 6, 1).timestamp())
-  end = int(dt.datetime(2018, 6, 10).timestamp())
+    start = int(dt.datetime(2018, 6, 1).timestamp())
+    end = int(dt.datetime(2018, 6, 30).timestamp())
 
-  gen = api.search_submissions(after=start, before=end, subreddit='singapore')
-  results = list(gen)
+    gen = api.search_submissions(
+        after=start, before=end, subreddit='singapore')
+    results = list(gen)
 
-  print(len(results))
+    print(len(results))
 
-  data = {}
-  data['titles'] = []
+    data = {}
+    data['titles'] = []
 
-  for result in results:
-      data['titles'].extend(tokenize(result.title))
+    for result in results:
+        data['titles'].extend(tokenize(result.title))
 
+    freq = nltk.FreqDist(data['titles'])
 
-  freq = nltk.FreqDist(data['titles'])
-  
-  print(freq.most_common(50))
-  return make_object_array(freq.most_common(50))
+    print(freq.most_common(50))
+    return make_object_array(freq.most_common(50))
