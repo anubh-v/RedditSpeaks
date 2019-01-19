bd976f70f740cd5f7227aec9859f06efb639b828
anubh-v <e0176401@u.nus.edu>
2019-01-19 18:34:57 +0800

    Abstract code for data extraction into a method

--- modules/extract.py
+++ modules/extract.py
@@ -4,27 +4,26 @@ import nltk
 import json
 from tokenizer import tokenize
 
-api = PushshiftAPI()
+def extract_data():
+  api = PushshiftAPI()
 
-start = int(dt.datetime(2019, 1, 1).timestamp())
-end = int(dt.datetime(2019, 1, 19).timestamp())
+  start = int(dt.datetime(2018, 6, 1).timestamp())
+  end = int(dt.datetime(2018, 6, 10).timestamp())
+
+  gen = api.search_submissions(after=start, before=end, subreddit='worldnews')
+  results = list(gen)
+
+  print(len(results))
+
+  data = {}
+  data['titles'] = []
+
+  for result in results:
+      data['titles'].extend(tokenize(result.title))
 
 
-gen = api.search_submissions(after=start, before=end, subreddit='singapore')
-results = list(gen)
+  freq = nltk.FreqDist(data['titles'])
+  print(freq.most_common(50))
 
-print(len(results))
-
-data = {}
-data['titles'] = []
-
-for result in results:
-    print(result.title)
-    data['titles'].extend(tokenize(result.title))
-    print(result.selftext)
-
-freq = nltk.FreqDist(data['titles'])
-print(freq.most_common(50))
-
-with open('results.json', 'w') as outfile:
-    json.dump(data, outfile)
+  with open('results.json', 'w') as outfile:
+      json.dump(data, outfile)
