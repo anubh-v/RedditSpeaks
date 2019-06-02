398da18e34aae60e15fc7d90401447c5bd318ad5
jhengy <jhy8890948@gmail.com>
2019-06-02 18:13:36 +0800

    Create WordCloud component and integrate in Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react"
 import WordCloud from "./WordCloud"
 class Home extends Component {
+  state = {
+    freqDist: []
+  }
+
+  componentDidMount = () => {
+    console.log("Home component mounted")
+  }
+
   render() {
     return (
       <div>
         <div className="container home">
-          <h4 className="center">Home</h4>
-          <WordCloud className="wordcloud" />
+          <h2 className="center">Home</h2>
+          <WordCloud className="container wordcloud" />
         </div>
       </div>
     )
--- client/src/components/WordCloud.js
+++ client/src/components/WordCloud.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react"
-
+import D3WordCloud from "react-d3-cloud"
 class WordCloud extends Component {
   state = {
-    freqDist: [] // array of objects
+    freqDist: [
+      { text: "Hey", value: 1000 },
+      { text: "lol", value: 200 },
+      { text: "first impression", value: 800 },
+      { text: "very cool", value: 1000000 },
+      { text: "duck", value: 10 }
+    ] // array of objects
   }
 
   render = () => {
-    return <div id="myChart" className="container wordcloud" />
+    const fontSizeMapper = word => Math.log2(word.value) * 5
+    const rotate = word => word.value % 360
+    return (
+      <div id="myChart" className="container">
+        <h5 className="wordcloud-title center"> Generated WordCloud </h5>
+        <D3WordCloud
+          data={this.state.freqDist}
+          fontSizeMapper={fontSizeMapper}
+          rotate={rotate}
+        />
+      </div>
+    )
   }
 }
 
