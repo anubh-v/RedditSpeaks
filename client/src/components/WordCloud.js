dc756be4ed03184cf58c2d64571b87953186c0b4
jhengy <jhy8890948@gmail.com>
2019-06-02 18:57:29 +0800

    conditional renderingof wordCloud template

--- client/src/components/WordCloud.js
+++ client/src/components/WordCloud.js
@@ -14,7 +14,11 @@ class WordCloud extends Component {
   render = () => {
     const fontSizeMapper = word => Math.log2(word.value) * 5
     const rotate = word => word.value % 360
-    return (
+    return this.props.freqDist.length === 0 ? (
+      <div id="myChart" className="container">
+        <h5 className="wordcloud-title center"> Waiting for backend... </h5>
+      </div>
+    ) : (
       <div id="myChart" className="container">
         <h5 className="wordcloud-title center"> Generated WordCloud </h5>
         <D3WordCloud
