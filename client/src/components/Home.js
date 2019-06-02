a3de0fba40d9a90a39b58884aba1925169ed5ce6
jhengy <jhy8890948@gmail.com>
2019-06-02 17:01:29 +0800

    create about and home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container home">
+          <h4 className="center">Home</h4>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
