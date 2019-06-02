5c494c700324f1107c8bb14bb0e6c6e63cfb409c
jhengy <jhy8890948@gmail.com>
2019-06-02 17:27:09 +0800

    Create a Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import React from "react"
 import { Route, BrowserRouter, Switch } from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <h1 className="title">Welcome to RedditSpeaks</h1>
-        </header>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
