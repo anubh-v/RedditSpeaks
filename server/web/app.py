9f3566251fe891c85c1cd7eb4ae701ce718df7a4
jhengy <jhy8890948@gmail.com>
2019-06-02 16:36:16 +0800

    Set up sanity check route

--- server/web/app.py
+++ server/web/app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask import render_template
+from flask_cors import CORS
 from modules.extract import extract_data
 
+# instantiate the app
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
 
 
 @app.route("/")
